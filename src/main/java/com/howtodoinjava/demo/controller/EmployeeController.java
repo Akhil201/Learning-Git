6fa7a18aa55bb1d6a26ef7cc4c7044d7b433c137
Akhilesh <akhil.btech2013@gmail.com>
2022-02-11 23:19:26 +0530

    Created MethodNmae

--- src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
+++ src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.howtodoinjava.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +12,22 @@ import com.howtodoinjava.demo.model.Employee;
 @RestController
 public class EmployeeController {
 	
-	@RequestMapping("/")
+	@GetMapping("/")
     public List<Employee> getEmployees() 
     {
 		List<Employee> employeesList = new ArrayList<Employee>();
+		
 		employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com"));
+		
 		return employeesList;
     }
-
+	@GetMapping("/")
+    public List<Employee> addEmployee() 
+    {
+		List<Employee> employeesList = new ArrayList<Employee>();
+		
+		employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com"));
+		
+		return employeesList;
+    }
 }
